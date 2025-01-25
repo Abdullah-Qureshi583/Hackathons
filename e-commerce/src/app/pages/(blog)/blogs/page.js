230e5f64848c14f5103a71113fd8c573c7142ddc
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2025-01-25 11:03:43 +0500

    Remove e-commerce

--- e-commerce/src/app/pages/(blog)/blogs/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Blogs from "./components/Blogs";
-import BlogSidebar from "./components/BlogSidebar";
-
-const BlogsPag = () => {
-  return <Blogs />;
-};
-
-export default BlogsPag;
